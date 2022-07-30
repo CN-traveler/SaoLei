389b10fd5055536cc6217eb65c5c5b5dac9f6653
CN-traveler <cn-traveler@qq.com>
2022-07-30 10:55:51 +0800

    统一代码风格

--- SaoLei/MainWindow.xaml.cpp
+++ SaoLei/MainWindow.xaml.cpp
@@ -31,122 +31,144 @@ namespace winrt::SaoLei::implementation
     }
 
     // 地图宽度
-    int map_width;
+    int mapWidth;
     // 地图高度
-    int map_height;
+    int mapHeight;
     // 地图雷数
-    int map_num;
+    int mapNum;
     // 地图数组 -1 雷 / 0-8 周围雷数
     int** map;
     // 随机交换地图数组的值到指定坐标
-    void map_swap(int x, int y) {
-        int rx = rand() % map_width;
-        int ry = rand() % map_height;
+    void mapSwap(int x, int y)
+    {
+        int rx = rand() % mapWidth;
+        int ry = rand() % mapHeight;
         int t = map[y][x];
         map[y][x] = map[ry][rx];
         map[ry][rx] = t;
         return;
     }
     // 开始游戏
-    void MainWindow::start_game()
+    void MainWindow::startGame()
     {
         // 地图格数
-        int map_sum = map_width * map_height;
+        int mapSum = mapWidth * mapHeight;
         // 地图雷数异常处理
-        if (map_num > map_sum) {
-            map_num = map_sum;
+        if (mapNum > mapSum)
+        {
+            mapNum = mapSum;
         }
-        if (map_num < 1) {
-            map_num = 1;
+        if (mapNum < 1)
+        {
+            mapNum = 1;
         }
         // 地图数组初始化
-        if (map) {
+        if (map != NULL)
+        {
             delete map;
         }
-        map = new int* [map_height];
+        map = new int* [mapHeight];
         // 初始化地图数组
-        for (int i = 0; i < map_height; i++) {
-            map[i] = new int[map_width];
-            for (int j = 0; j < map_width; j++) {
-                if (i * map_height + j < map_num) {
+        for (int i = 0; i < mapHeight; i++)
+        {
+            map[i] = new int[mapWidth];
+            for (int j = 0; j < mapWidth; j++)
+            {
+                if (i * mapHeight + j < mapNum)
+                {
                     map[i][j] = -1;
                 }
-                else {
+                else
+                {
                     map[i][j] = 0;
                 }
             }
         }
         // 随机打乱地图数组
-        for (int i = 0; i < map_height; i++) {
-            for (int j = 0; j < map_width; j++) {
-                map_swap(j, i);
+        for (int i = 0; i < mapHeight; i++)
+        {
+            for (int j = 0; j < mapWidth; j++)
+            {
+                mapSwap(j, i);
             }
         }
         // 清理地图
-        map_panel().Children().Clear();
+        mapPanel().Children().Clear();
         // 生成地图
-        for (int i = 0; i < map_height; i++) {
+        for (int i = 0; i < mapHeight; i++)
+        {
             StackPanel rowPanel = StackPanel();
             rowPanel.Orientation(Orientation::Horizontal);
-            for (int j = 0; j < map_width; j++) {
+            for (int j = 0; j < mapWidth; j++)
+            {
                 Button button = Button();
                 button.Width(40);
                 button.Height(40);
-                if (map[i][j] >= 0) {
-                    if (i > 0) {
+                if (map[i][j] >= 0)
+                {
+                    if (i > 0)
+                    {
                         map[i][j] += (map[i - 1][j] == -1) ? 1 : 0;
-                        if (j > 0) {
+                        if (j > 0)
+                        {
                             map[i][j] += (map[i - 1][j - 1] == -1) ? 1 : 0;
                             map[i][j] += (map[i][j - 1] == -1) ? 1 : 0;
                         }
-                        if (j < map_width - 1) {
+                        if (j < mapWidth - 1)
+                        {
                             map[i][j] += (map[i - 1][j + 1] == -1) ? 1 : 0;
                             map[i][j] += (map[i][j + 1] == -1) ? 1 : 0;
                         }
                     }
-                    else {
-                        if (j > 0) {
+                    else 
+                    {
+                        if (j > 0)
+                        {
                             map[i][j] += (map[i][j - 1] == -1) ? 1 : 0;
                         }
-                        if (j < map_width - 1) {
+                        if (j < mapWidth - 1)
+                        {
                             map[i][j] += (map[i][j + 1] == -1) ? 1 : 0;
                         }
                     }
-                    if (i < map_height - 1) {
+                    if (i < mapHeight - 1)
+                    {
                         map[i][j] += (map[i + 1][j] == -1) ? 1 : 0;
-                        if (j > 0) {
+                        if (j > 0)
+                        {
                             map[i][j] += (map[i + 1][j - 1] == -1) ? 1 : 0;
                         }
-                        if (j < map_width - 1) {
+                        if (j < mapWidth - 1)
+                        {
                             map[i][j] += (map[i + 1][j + 1] == -1) ? 1 : 0;
                         }
                     }
                 }
-                button.Name(to_hstring(i * map_width + j));
+                button.Name(to_hstring(i * mapWidth + j));
                 Color ccc = Color();
                 ccc.A = 127;
-                ccc.R = map_width * 5;
-                ccc.G = map_height * 10;
+                ccc.R = mapWidth * 5;
+                ccc.G = mapHeight * 10;
                 ccc.B = 204;
                 SolidColorBrush cccSolidColorBrush = SolidColorBrush(ccc);
                 button.Background(cccSolidColorBrush);
-                button.Click({ this, &MainWindow::game_step });
+                button.Click({ this, &MainWindow::gameStep });
                 rowPanel.Children().Append(button);
             }
-            map_panel().Children().Append(rowPanel);
+            mapPanel().Children().Append(rowPanel);
         }
         // 结束
         return;
     }
     // 走游戏步
-    void MainWindow::game_step(IInspectable const& sender, RoutedEventArgs const& e)
+    void MainWindow::gameStep(IInspectable const& sender, RoutedEventArgs const& e)
     {
         // 获取点击对象
         Button button = sender.as<Button>();
         // 获取坐标
         int index = stoi(to_string(button.Name()));
-        int i = index / map_width;
-        int j = index % map_width;
+        int i = index / mapWidth;
+        int j = index % mapWidth;
         // 设置点击背景
         Color fff = Color();
         fff.A = 255;
@@ -155,50 +177,64 @@ namespace winrt::SaoLei::implementation
         fff.B = 255;
         SolidColorBrush fffSolidColorBrush = SolidColorBrush(fff);
         // 检测是否已经点击
-        if (button.Background().as< SolidColorBrush>().Color() == fff) {
+        if (button.Background().as< SolidColorBrush>().Color() == fff)
+        {
             return;
         }
         // 检测是否为雷
-        if (map[i][j] != -1) {
+        if (map[i][j] != -1)
+        {
             // 显示点击后的背景
             button.Background(fffSolidColorBrush);
             // 显示数值
-            if (map[i][j] != 0) {
+            if (map[i][j] != 0)
+            {
                 button.Content(box_value(map[i][j]));
             }
         }
-        else {
+        else
+        {
             // 重新开始
-            start_game();
+            startGame();
         }
         // 批量打开
-        if (map[i][j] == 0) {
-            if (i > 0) {
-                game_step(map_panel().FindName(to_hstring((i - 1) * map_width + j)), NULL);
-                if (j > 0) {
-                    game_step(map_panel().FindName(to_hstring((i - 1) * map_width + j - 1)), NULL);
-                    game_step(map_panel().FindName(to_hstring(i * map_width + j - 1)), NULL);
+        if (map[i][j] == 0)
+        {
+            if (i > 0)
+            {
+                gameStep(mapPanel().FindName(to_hstring((i - 1) * mapWidth + j)), NULL);
+                if (j > 0)
+                {
+                    gameStep(mapPanel().FindName(to_hstring((i - 1) * mapWidth + j - 1)), NULL);
+                    gameStep(mapPanel().FindName(to_hstring(i * mapWidth + j - 1)), NULL);
                 }
-                if (j < map_width - 1) {
-                    game_step(map_panel().FindName(to_hstring((i - 1) * map_width + j + 1)), NULL);
-                    game_step(map_panel().FindName(to_hstring(i * map_width + j + 1)), NULL);
-                }
-            }
-            else {
-                if (j > 0) {
-                    game_step(map_panel().FindName(to_hstring(i * map_width + j - 1)), NULL);
-                }
-                if (j < map_width - 1) {
-                    game_step(map_panel().FindName(to_hstring(i * map_width + j + 1)), NULL);
+                if (j < mapWidth - 1)
+                {
+                    gameStep(mapPanel().FindName(to_hstring((i - 1) * mapWidth + j + 1)), NULL);
+                    gameStep(mapPanel().FindName(to_hstring(i * mapWidth + j + 1)), NULL);
                 }
             }
-            if (i < map_height - 1) {
-                game_step(map_panel().FindName(to_hstring((i + 1) * map_width + j)), NULL);
-                if (j > 0) {
-                    game_step(map_panel().FindName(to_hstring((i + 1) * map_width + j - 1)), NULL);
+            else
+            {
+                if (j > 0)
+                {
+                    gameStep(mapPanel().FindName(to_hstring(i * mapWidth + j - 1)), NULL);
                 }
-                if (j < map_width - 1) {
-                    game_step(map_panel().FindName(to_hstring((i + 1) * map_width + j + 1)), NULL);
+                if (j < mapWidth - 1)
+                {
+                    gameStep(mapPanel().FindName(to_hstring(i * mapWidth + j + 1)), NULL);
+                }
+            }
+            if (i < mapHeight - 1)
+            {
+                gameStep(mapPanel().FindName(to_hstring((i + 1) * mapWidth + j)), NULL);
+                if (j > 0)
+                {
+                    gameStep(mapPanel().FindName(to_hstring((i + 1) * mapWidth + j - 1)), NULL);
+                }
+                if (j < mapWidth - 1)
+                {
+                    gameStep(mapPanel().FindName(to_hstring((i + 1) * mapWidth + j + 1)), NULL);
                 }
             }
         }
@@ -206,7 +242,7 @@ namespace winrt::SaoLei::implementation
         return;
     }
     // 修改难度
-    void MainWindow::change_difficulty(NavigationView const& sender, NavigationViewSelectionChangedEventArgs const& args)
+    void MainWindow::changeDifficulty(NavigationView const& sender, NavigationViewSelectionChangedEventArgs const& args)
     {
         // 获取对象
         NavigationView navigationView = sender.as<NavigationView>();
@@ -214,49 +250,55 @@ namespace winrt::SaoLei::implementation
         // 获取难度
         hstring difficulty = to_hstring(navigationViewItem.Name());
         // 难度处理
-        if (difficulty == L"custom") {
+        if (difficulty == L"custom")
+        {
             // 显示自定义配置
             visibleFooterPane(NULL, NULL);
             // 获取自定义难度值
-            map_width = width().Value();
-            map_height = height().Value();
-            map_num = num().Value();
+            mapWidth = width().Value();
+            mapHeight = height().Value();
+            mapNum = num().Value();
         }
-        else {
+        else
+        {
             //隐藏自定义配置
             collapsedFooterPane(NULL, NULL);
             // 获取对应的难度值
-            if (difficulty == L"easy") {
-                map_width = 9;
-                map_height = 9;
-                map_num = 10;
+            if (difficulty == L"easy")
+            {
+                mapWidth = 9;
+                mapHeight = 9;
+                mapNum = 10;
             }
-            else if (difficulty == L"normal") {
-                map_width = 16;
-                map_height = 16;
-                map_num = 40;
+            else if (difficulty == L"normal")
+            {
+                mapWidth = 16;
+                mapHeight = 16;
+                mapNum = 40;
             }
-            else if (difficulty == L"difficulty") {
-                map_width = 30;
-                map_height = 16;
-                map_num = 99;
+            else if (difficulty == L"difficulty")
+            {
+                mapWidth = 30;
+                mapHeight = 16;
+                mapNum = 99;
             }
         }
         // 开始游戏
-        start_game();
+        startGame();
     }
     // 自定义难度值修改 监听
-    void MainWindow::custom_change(IInspectable const& sender, Primitives::RangeBaseValueChangedEventArgs const& e)
+    void MainWindow::customChange(IInspectable const& sender, Primitives::RangeBaseValueChangedEventArgs const& e)
     {
         // 判断是否完成初始化
-        if (width() != NULL && height() != NULL && num() != NULL) {
+        if (width() != NULL && height() != NULL && num() != NULL)
+        {
             // 获取自定义难度值
-            map_width = width().Value();
-            map_height = height().Value();
-            num().Maximum(map_width * map_height);
-            map_num = num().Value();
+            mapWidth = width().Value();
+            mapHeight = height().Value();
+            num().Maximum(mapWidth * mapHeight);
+            mapNum = num().Value();
             // 开始游戏
-            start_game();
+            startGame();
         }
     }
     // 隐藏自定义配置
--- SaoLei/MainWindow.xaml.h
+++ SaoLei/MainWindow.xaml.h
@@ -9,10 +9,10 @@ namespace winrt::SaoLei::implementation
         MainWindow();
         int32_t MyProperty();
         void MyProperty(int32_t value);
-        void start_game();
-        void game_step(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
-        void change_difficulty(winrt::Microsoft::UI::Xaml::Controls::NavigationView const& sender, winrt::Microsoft::UI::Xaml::Controls::NavigationViewSelectionChangedEventArgs const& args);
-        void custom_change(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs const& e);
+        void startGame();
+        void gameStep(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        void changeDifficulty(winrt::Microsoft::UI::Xaml::Controls::NavigationView const& sender, winrt::Microsoft::UI::Xaml::Controls::NavigationViewSelectionChangedEventArgs const& args);
+        void customChange(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs const& e);
         void collapsedFooterPane(winrt::Microsoft::UI::Xaml::Controls::NavigationView const& sender, winrt::Windows::Foundation::IInspectable const& args);
         void visibleFooterPane(winrt::Microsoft::UI::Xaml::Controls::NavigationView const& sender, winrt::Windows::Foundation::IInspectable const& args);
     };
